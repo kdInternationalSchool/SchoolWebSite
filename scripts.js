39c9081c56ad89f1309588127c9b03d4ecc86f21
Anand <anand.kumar@lybl.com>
2026-02-01 21:19:08 +0530

    added

--- scripts.js
+++ scripts.js
@@ -1,10 +1,23 @@
-function scrollTo(id) {
-    document.getElementById(id).scrollIntoView({ behavior: "smooth" });
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let slides = document.getElementsByClassName("slides");
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
   }
-  
-  function submitForm(e) {
-    e.preventDefault();
-    document.getElementById("msg").innerText =
-      "Thank you! We will contact you shortly.";
-  }
-  
+  slideIndex++;
+  if (slideIndex > slides.length) { slideIndex = 1 }
+  slides[slideIndex - 1].style.display = "block";
+  setTimeout(showSlides, 4000); // change image every 4 sec
+}
+
+function scrollToSection(id) {
+  document.getElementById(id).scrollIntoView({ behavior: "smooth" });
+}
+
+function submitForm(e) {
+  e.preventDefault();
+  document.getElementById("msg").innerText =
+    "Thank you! Our team will contact you soon.";
+}
